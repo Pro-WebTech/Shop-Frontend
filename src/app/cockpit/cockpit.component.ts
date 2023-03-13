fb09e00090801d1b7f0872c19d82405f97e35aa2
Super-Smile <baydev1998@gmail.com>
2023-03-13 23:41:18 +0900

    local preference

--- src/app/cockpit/cockpit.component.ts
+++ src/app/cockpit/cockpit.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Output, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-cockpit',
@@ -8,24 +8,23 @@ import { Component, EventEmitter, Output } from '@angular/core';
 export class CockpitComponent {
   @Output() serverCreated = new EventEmitter<{serverName: string, serverContent: string}>();
   @Output('bpCreated') blueprintCreated = new EventEmitter<{serverName: string, serverContent: string}>();
-  newServerName = '';
-  newServerContent = '';
-  onAddServer() {
+  // newServerName = '';
+  // newServerContent = '';
 
-    this.serverCreated.emit({serverName: this.newServerName, serverContent: this.newServerContent});
-    // this.serverElements.push({
-    //   type: 'server',
-    //   name: this.newServerName,
-    //   content: this.newServerContent
-    // });
+  @ViewChild('serverContentInput') serverContentInput: ElementRef;
+
+  onAddServer(nameInput: HTMLInputElement) {
+    console.log(this.serverContentInput)
+    this.serverCreated.emit({
+      serverName: nameInput.value, 
+      serverContent: this.serverContentInput.nativeElement.value});
+    
   }
 
-  onAddBlueprint() {
-    this.blueprintCreated.emit({serverName: this.newServerName, serverContent: this.newServerContent});
-    // this.serverElements.push({
-    //   type: 'blueprint',
-    //   name: this.newServerName,
-    //   content: this.newServerContent
-    // });
+  onAddBlueprint(nameInput: HTMLInputElement) {
+     this.blueprintCreated.emit({
+      serverName: nameInput.value, 
+      serverContent: this.serverContentInput.nativeElement.value});
+    
   }
 }
